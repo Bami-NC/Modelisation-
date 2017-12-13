3c9eac9872ee8f6e53e272b6e1088aaeb2a453af
Bami-NC <antoine.fremy@gmail.com>
2017-12-13 08:48:55 +0100

    Changement de la donnée masse volumique-résultats cohérents

--- donnees_PDC.f90
+++ donnees_PDC.f90
@@ -2,24 +2,24 @@
 0.007   !Rayon interne [m]
 5       !Longueur de la conduite [m]
 0.002   !épaisseur du PER [m]
-0.0003  !épaisseur de l'isolant XPS[m]
+0.008  !épaisseur de l'isolant XPS[m]
 
 500000   !Nombre de pas de temps
 5000     !durée de simulation [s]
 1000     !Temps de sauvegarde [s]
 
-2       !vitesse dans la conduite [m/s]
-0.998   !masse volumique moyenne de l'ECS (à 40°C) [kg/m3]
+0.9       !vitesse dans la conduite [m/s]
+998     !masse volumique moyenne de l'ECS (à 40°C) [kg/m3]
 4184    !Capacité calorifique moyenne de l'eau (à 40°C) [J/kg.K]
 2300    !Capacité calorifique moyenne du PER (à 40°C) [J/kg.K]
 0.640   !Conductivité de l'ECS (à 40°C) [W/m.K]
-3000    !Coefficient de convection (pour de l'eau en régime turbulent) [W/m.K]
+1000    !Coefficient de convection (pour de l'eau en régime turbulent) [W/m.K]
 
 0.4     !Conductivité du PER [W/m.K]
 940     !masse volumique moyenne du PER (à 40°C) [g/cm3]
 
 0.04    !Conductivité de XPS [W/m.K]
-5       !Coefficient de convection naturelle pour de l'air [W/m.K]
+10      !Coefficient de convection naturelle pour de l'air [W/m.K]
 20      !Température ambiante [°C]
 65      !Température du préparateur [°C]
 
